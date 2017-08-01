f49acfad48e937704fd832322628a52cb81468cf
LuksSouza <lucas-machado@outlook.com>
2017-08-01 07:34:39 -0300

    Corrigido importação de arquivo fotos-controller.js na index.html.

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,7 +1,6 @@
-angular.module('alurapic').
-    controller('FotosController', function($scope) { 
-        $scope.foto = {
-            titulo: 'Leão',
-            url: 'http://www.fundosanimais.com/Minis/leoes.jpg'            
-        };
+angular.module('alurapic').controller('FotosController', function($scope) { 
+    $scope.foto = {
+        titulo: 'Leão',
+        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'            
+    };
 });
