27fc1de30625594401b34a18a9daf201da1f63af
LuksSouza <lucas-machado@outlook.com>
2017-08-18 10:07:17 -0300

    Melhorado a view do index com classes do bootstrap;
    Criação de um controller fotos-controller que irá requisitar ao back e retornar para a view um JSON com uma lista de objetos foto;

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,15 @@
-angular.module('alurapic').controller('FotosController', function($scope) { 
-    $scope.foto = {
-        titulo: 'Leão',
-        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'            
-    };
+angular.module('alurapic').controller('FotosController', function($scope, $http) { 
+    
+    //Array que irá retornar para a view com os objetos retornado do back
+    $scope.fotos = [];
+
+    //requisição get que, se sucesso, retornará um array com os objetos javascript (JSON) do back
+    $http.get('/v1/fotos')
+    .success(function (retorno) {
+        $scope.fotos = retorno;
+    })
+    .error(function(error) {
+        console.log(error);
+    });
+
 });
