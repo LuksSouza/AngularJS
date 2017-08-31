9ce9f74ccdeb0ce383a32d0803e34cd22ff262bb
LuksSouza <lucas-machado@outlook.com>
2017-08-31 08:20:43 -0300

    Adicionado diretiva para remover da tela que lista as imagens retornada pelo back-end;

--- alurapic/public/js/directives/minhas-diretivas.js
+++ alurapic/public/js/directives/minhas-diretivas.js
@@ -29,4 +29,19 @@ angular.module('minhasDiretivas', [])
         ddo.templateUrl = "js/directives/minhas-fotos.html";
         
         return ddo;        
+    })
+    .directive('meuBotaoPerigo', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "E";
+
+        ddo.scope = {
+            nome: '@',
+            acao : '&'
+        }
+        
+        ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
+
+        return ddo;
     });
